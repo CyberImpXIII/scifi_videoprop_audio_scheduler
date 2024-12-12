f120051a06377b22b8f55311cedf52a5746ab746
CyberImpXIII <jnelsenepstein@gmail.com>
2024-12-13 03:21:55 +0900

    removed slider in favor of prefab

--- src/components/Slider.js
+++
@@ -1,9 +0,0 @@
-const Slider = ({sliderVal, setSliderVal})=>{
-    <div class="slidecontainer">
-    <input type="range" min="1" max="100" value={sliderVal} className="slider" id="myRange" onChange={(e)=>{
-      console.log(e.target.value)
-      setSliderVal(e.target.value)}}/>
-  </div>
-}
-
-export default Slider
