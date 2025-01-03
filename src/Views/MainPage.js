4841f37f78a507b3a76b14b43302873cf6e5ac01
CyberImpXIII <jnelsenepstein@gmail.com>
2025-01-03 14:30:36 +0900

    added ui elements

--- src/Views/MainPage.js
+++ src/Views/MainPage.js
@@ -2,18 +2,28 @@ import { useState, useEffect, useRef, createRef } from "react"
 import Slider from 'rc-slider';
 import 'rc-slider/assets/index.css';
 import '../styles/mainPage.css'
-const MainPage = ({Xincrease, setXincrease, XincreaseSource, setXincreaseSource, Yincrease, setYincrease, YincreaseSource, setYincreaseSource, setConfig, config, rulesArray, mediaArray, baseContext, pollingSpeed, pollingSpeedSource, buttonText})=>{
+import Chart from "react-apexcharts";
+import options from '../shared/options'
+
+const MainPage = ({Xincrease, setXincrease, XincreaseSource, setXincreaseSource, Yincrease, setYincrease, YincreaseSource, setYincreaseSource, setConfig, config, rulesArray, mediaArray, baseContext, pollingSpeed, pollingSpeedSource, buttonTextOne, buttonTextTwo})=>{
     const [sliderVal, setSliderVal] = useState(50);
     const [elementsContext, setElementsContext] = useState([]);
     const [elementsRef, setElementsRef] = useState(useRef(mediaArray.map(() => createRef())));
-    const [amount, setAmount] = useState(0);
     const [positionArray, setPositionArray] = useState([[0,0]])
     const [stopArray, setStopArray] = useState([])
+    const [lineGraphWidth, setLineGraphWidth] = useState(0)
+    const [lineGraphHeight, setLineGraphHeight] = useState(0)
+    const [radialGraphWidth, setRadialGraphWidth] = useState(0)
+    const [radialGraphHeight, setRadialGraphHeight] = useState(0)
+    const [lineGraphPercent, setLineGraphPercent] = useState(0)
+    const [over95, setOver95] = useState(false)
+    const [over90, setOver90] = useState(false)
+    const [over70, setOver70] = useState(false)
+    const [over50, setOver50] = useState(false)
 
-    const pollingHandler = ()=>{
-        setPositionArray((prevPositionArray)=>[...prevPositionArray, [prevPositionArray.length, prevPositionArray.length]])
-        setAmount((prevAmount)=> prevAmount + Yincrease)
-    }
+    useEffect(()=>{
+        setLineGraphPercent(Math.round((positionArray[positionArray.length - 1][1] / lineGraphHeight) * 100))
+    },[positionArray])
 
     const speedHandler = ()=>{
         if(pollingSpeedSource === 'numerical'){
@@ -23,11 +33,53 @@ const MainPage = ({Xincrease, setXincrease, XincreaseSource, setXincreaseSource,
         }
     }
 
-    useEffect(()=>{
-        const myInterval = setInterval(pollingHandler, speedHandler());
-        return ()=>clearInterval(myInterval);
-    },[pollingSpeedSource, sliderVal])
+   
 
+    useEffect(()=>{
+        if(!over50){
+            const myInterval = setInterval(()=>{
+                setPositionArray((prevPositionArray)=>[...prevPositionArray, [prevPositionArray.length, prevPositionArray.length]])
+            }, speedHandler());
+            return ()=>clearInterval(myInterval);
+        }else if(over50 && !over70){
+            const myInterval = setInterval(()=>{
+
+            }, speedHandler());
+            return ()=>clearInterval(myInterval);
+        }else if (over70 && !over90){
+            const myInterval = setInterval(()=>{
+
+            }, speedHandler());
+            return ()=>clearInterval(myInterval);
+        }else if (over90 && !over95){
+            console.log('over90')
+            const myInterval = setInterval(()=>{
+                setPositionArray((prevPositionArray)=>[...prevPositionArray, [prevPositionArray[prevPositionArray.length - 1][0], prevPositionArray[prevPositionArray.length - 1][1]]]) 
+            }, speedHandler());
+            return ()=>clearInterval(myInterval);
+        }else if (over95){
+            const myInterval = setInterval(()=>{
+                
+            }, speedHandler());
+            return ()=>clearInterval(myInterval);
+        }
+    },[pollingSpeedSource, sliderVal, over90, over50, over70, over95])
+
+    useEffect(()=>{
+        if(lineGraphPercent > 50){
+            setOver50(true)
+        }
+        if(lineGraphPercent > 70){
+            setOver70(true)
+        }
+        if(lineGraphPercent > 90){
+            setOver90(true)
+        }
+        if(lineGraphPercent > 95){
+            setOver95(true)
+        }
+    },[lineGraphPercent])
+    
     useEffect(() => {
         if (Array.isArray(elementsRef.current) && elementsRef.current[0] && elementsRef.current[0].current !== null && elementsContext.length === 0) {
             let tempElementsContext = []
@@ -129,7 +181,6 @@ const MainPage = ({Xincrease, setXincrease, XincreaseSource, setXincreaseSource,
 
 
     return(
-
         <>
             <button onClick={playButtonHandler} key='startbutton'>Start  </button>
             <button key='stopbutton' onClick={()=>{stopHandler(stopArray)}}>Stop </button>
@@ -143,21 +194,32 @@ const MainPage = ({Xincrease, setXincrease, XincreaseSource, setXincreaseSource,
                 </div>
 
                 <div key='svgparent' style={{display:'flex', flexDirection:'column', width:'30vw', border:'solid black 1px', marginTop:'35px'}}>
-                    <div id='radialGraph' style={{width:'100%', height:'50%'}}></div>
-                    <div id='lineGraph' style={{width:'100%', height:'50%'}}>
-                        <svg style={{fill:'black', stroke:'black'}}>
+                    <div id='radialGraph' ref={(node)=>{setRadialGraphWidth(node?.offsetWidth); setRadialGraphHeight(node?.offsetHeight)}} style={{width:'100%', height:'50%', display:'flex', flexDirection:'column', justifyContent:'space-around'}}>
+                        <Chart width={radialGraphWidth} height={radialGraphHeight} style={{marginTop:'-35px'}} options={options(lineGraphPercent)} series={[lineGraphPercent]} type="radialBar" />
+                    </div>
+                    <div id='lineGraph' ref={(node)=>{setLineGraphWidth(node?.offsetWidth); setLineGraphHeight(node?.offsetHeight)}}>
+                        <svg style={{fill:'blue', stroke:'blue', position:'absolute', left:'0', width:'100%', height:'100%' }}>
                             {positionArray.map((element, position, fullArray)=>{
                                 return(position === 0 ? 
                                 <path key={`startpath`} style={{fill:'black', stroke:'black'}} d='M0 0'/> :
-                                <path key={`${position}path`} style={{fill:'black', stroke:'black'}} d={`M${fullArray[position-1][0]} ${fullArray[position-1][1]} L${element[0]} ${element[1]}`}/>)
+                                <path key={`${position}path`} style={{fill:'blue', stroke:'blue'}} d={`M${fullArray[position-1][0]} ${fullArray[position-1][1]} L${element[0]} ${element[1]}`}/>)
                             })}
                         </svg>
                     </div>
 
                 </div>
-                <div id='buttonParent'>
-                    <button className='button'></button>
-                    {buttonText}
+                <div id='buttonParent' style={{display:'flex', flexDirection:'column', width:'30vw', marginTop:'35px', marginRight:'5vw', position:'relative'}}>
+                    <div className='top' style={{width:'100%', height:'50%'}}></div>
+                    <div className="bottom" style={{width:'100%', height:'50%'}}>
+                        <div className='buttonContainer'>
+                            {buttonTextOne}
+                            <button className='button'></button>
+                        </div>
+                        <div className='buttonContainer' style={{position:'absolute', right:'0'}}>
+                            {buttonTextTwo}
+                            <button className='button'></button>
+                        </div>
+                    </div>
                 </div>
             </div>
             
